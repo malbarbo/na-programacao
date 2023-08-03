c69b71c3238ebf4590313df06e7f89e000c9ffd3
Marco A L Barbosa <malbarbo@gmail.com>
2023-08-03 14:48:39 -0300

    projeto de funções: adiciona a análise e tipos de dados aos exemplos

--- 06-projeto-de-programas/exemplos/custo_viagem.py
+++ 06-projeto-de-programas/exemplos/custo_viagem.py
@@ -1,3 +1,13 @@
+# Análise
+#
+# Calcular o custo em reais para percorrer uma determinada distância levando em
+# consideração o desempenho do carro e o preço do litro do combustível.
+
+# Tipos de Dados
+#
+# Distância, redimento, preço do litro e custo são representado por números
+# positivos.
+
 def custo_viagem(distancia: float, rendimento: float, preco: float) -> float:
     '''
     Calcula o custo em reais para percorrer a *distancia* especificada
--- 06-projeto-de-programas/exemplos/numero_azulejos.py
+++ 06-projeto-de-programas/exemplos/numero_azulejos.py
@@ -1,9 +1,20 @@
 import math
 
+# Análise
+#
+# Calcular o número de azulejos necessários para azulejar uma parede com
+# determinado comprimento e altura, considerando que cada azulejo mede
+# 0,2m x 0,2m e que nenhum azulejo é perdido e que recortes são descartados.
+
+# Tipos de dados
+#
+# A largura e a altura são dados em metros e representadas com números
+# positivos.
+
 def numero_azulejos(comprimento: float, altura: float) -> int:
     '''
     Calcula o número de azulejos de 0,2mx0,2m necessários para azulejar uma
-    area de tamanho *comprimento* x *altura* (em metros) considerando que
+    pare de tamanho *comprimento* x *altura* (em metros) considerando que
     nenhum azulejo é perdido e que recortes são descartados.
 
     Exemplos
@@ -11,7 +22,8 @@ def numero_azulejos(comprimento: float, altura: float) -> int:
     120
     >>> numero_azulejos(1.5, 2.3)
     96
-    >>> # Alguns casos extremos
+
+    Algumas situações particulares
     >>> numero_azulejos(0.2, 0.2)
     1
     >>> numero_azulejos(0.3, 0.2)
