39dc29d8798a53affaa3b9d221bd442069975d0a
Marco A L Barbosa <malbarbo@gmail.com>
2023-08-07 11:39:12 -0300

    gen.py: evita arquivos antigos na geração dos zips
    Antes de gerar um arquivo zip, remove o antigo se existir.
    Isto evita que arquivos já removidos persistam dentro dos zips.

--- gen.py
+++ gen.py
@@ -30,7 +30,7 @@ for cap in sorted(os.listdir()):
             print(f'{target}: {cap}/{item}/*')
             print(f'\t@echo {target}')
             print(f'\t@mkdir -p {DEST}')
-            print(f'\t@zip {target} -x *.swp -x *.mypy* -r {cap}/{item}')
+            print(f'\t@rm {target}; zip {target} -x *.swp -x *.mypy* -r {cap}/{item}')
             print()
         elif item == 'notas-de-aula.md':
             base = os.path.splitext(item)[0]
