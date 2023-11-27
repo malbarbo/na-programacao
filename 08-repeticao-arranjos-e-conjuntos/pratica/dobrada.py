af9e53225b6e3def513e614a924461f92a4e6c23
Marco A L Barbosa <malbarbo@gmail.com>
2023-11-27 16:53:41 -0300

    repeticao: adiciona resolucao prática dobrada.py

---
+++ 08-repeticao-arranjos-e-conjuntos/pratica/dobrada.py
@@ -0,0 +1,28 @@
+def dobrada(lst: list[int]) -> bool:
+    '''
+    Produz True se *lst* é dobrada, isto é, pode
+    ser obtida pela concatenação de duas listas iguais.
+    Devolve False caso contrário.
+
+    Exemplos
+    >>> dobrada([])
+    True
+    >>> dobrada([3])
+    False
+    >>> dobrada([3, 3])
+    True
+    >>> dobrada([3, 2])
+    False
+    >>> dobrada([2, 6, 1, 2, 6, 1])
+    True
+    >>> dobrada([2, 6, 1, 2, 6, 1, 4])
+    False
+    '''
+    dobrada = len(lst) % 2 == 0
+    metade = len(lst) // 2
+    i = 0
+    while i < metade and dobrada:
+        if lst[i] != lst[i + metade]:
+            dobrada = False
+        i = i + 1
+    return dobrada
