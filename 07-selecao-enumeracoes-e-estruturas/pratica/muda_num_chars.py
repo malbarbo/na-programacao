ca55573c247d423d36700404515251d17f5f67fd
Marco A L Barbosa <malbarbo@gmail.com>
2023-11-28 12:55:36 -0300

    selecao: adiciona solução da prática muda_num_chars.py

---
+++ 07-selecao-enumeracoes-e-estruturas/pratica/muda_num_chars.py
@@ -0,0 +1,24 @@
+def muda_num_chars(s: str, n: int) -> str:
+    '''
+    Cria uma nova string com *n* caracteres a partir de *s*.
+    Se *s* tem menos que *n* caracteres, adiciona espaços no final.
+    Se *s* tem mais que *n* caracteres, remove os caracteres excedentes do final.
+    Se *s* tem *n* caracteres, devolve *s*.
+
+    Requer que n >= 0.
+
+    Exemplos
+    >>> muda_num_chars('casa', 7)
+    'casa   '
+    >>> muda_num_chars('computação', 4)
+    'comp'
+    >>> muda_num_chars('python', 6)
+    'python'
+    '''
+    if len(s) < n:
+        r = s + ' ' * (n - len(s))
+    elif len(s) > n:
+        r = s[:n]
+    else:
+        r = s
+    return r
