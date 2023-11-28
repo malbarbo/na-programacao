a4347ee36ea681e357ae0fa48cc7315148ce910c
Marco A L Barbosa <malbarbo@gmail.com>
2023-11-28 12:54:04 -0300

    repeticao: adiciona solução do problema string_ints.py

---
+++ 08-repeticao-arranjos-e-conjuntos/problemas/string_ints.py
@@ -0,0 +1,25 @@
+# Análise
+#
+# Identificar os inteiros em uma string com inteiros (sequência de dígitos) separados por vírgula.
+
+def string_ints(s: str) -> list[int]:
+    '''
+    Identifica os inteiros que estão em *s*.
+    Se *s* tem mais de um inteiro, então eles devem estar
+    separados por vírgula, como em '512,12,145'.
+
+    >>> string_ints('')
+    []
+    >>> string_ints('102')
+    [102]
+    >>> string_ints('512,12,145')
+    [512, 12, 145]
+    '''
+    ints = []
+    while ',' in s:
+        i = s.index(',')
+        ints.append(int(s[:i]))
+        s = s[i + 1:]
+    if s != '':
+        ints.append(int(s))
+    return ints
