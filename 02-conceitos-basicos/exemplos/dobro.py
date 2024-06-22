931e5e049f9c97151f7aca7754c90f7de7120b1f
Marco A L Barbosa <malbarbo@gmail.com>
2024-06-22 11:47:57 -0300

    conceitos básicos: ajusta o exemplo dobro.py e adiciona novo_seculo.py

--- 02-conceitos-basicos/exemplos/dobro.py
+++ 02-conceitos-basicos/exemplos/dobro.py
@@ -1,14 +1,3 @@
-def main():
-    # Entrada
-    numero: int = int(input('Digite um número inteiro: '))
-
-    # Processamento
-    d: int = dobro(numero)
-
-    # Saída
-    print('O dobro de', numero, 'é', d)
-
-
 def dobro(x: int) -> int:
     '''
     Calcula o dobro de *x*.
@@ -18,7 +7,3 @@ def dobro(x: int) -> int:
     8
     '''
     return 2 * x
-
-
-if __name__ == '__main__':
-    main()
