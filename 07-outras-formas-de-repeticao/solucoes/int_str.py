9c75be3d90c49b40fc9c8c20c8dc4e395d8f1533
Marco A L Barbosa <malbarbo@gmail.com>
2024-08-12 15:12:57 -0300

    outras-formas-de-repeticao: adiciona soluções int_str.py e pi.py

---
+++ 07-outras-formas-de-repeticao/solucoes/int_str.py
@@ -0,0 +1,27 @@
+def int_str(n: int) -> str:
+    '''
+    Converte *n* para uma string.
+    Requer que n >= 0.
+
+    Exemplos
+    >>> int_str(0)
+    '0'
+    >>> int_str(9)
+    '9'
+    >>> int_str(10)
+    '10'
+    >>> int_str(71620)
+    '71620'
+    >>> int_str(123456)
+    '123456'
+    '''
+    assert n >= 0
+    digitos = '0123456789'
+    if n == 0:
+        r = '0'
+    else:
+        r = ''
+        while n > 0:
+            r = digitos[n % 10] + r
+            n = n // 10
+    return r
